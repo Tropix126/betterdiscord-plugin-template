ab3c32d1ebf5a7b4bc71519ecaf3304b54aef65f
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-05-02 17:25:40 -0500

    simplify build script a little

--- scripts/build.js
+++ scripts/build.js
@@ -3,17 +3,6 @@ import config from "../betterdiscord.config.js";
 
 const args = process.argv.slice(2);
 
-async function build(args) {
-    const start = performance.now();
-    await esbuild.build({
-        watch: args.includes("--dev"),
-        banner: {
-            js: generateMeta(config.meta)
-        },
-        ...config.esbuild
-    });
-}
-
 function generateMeta(obj) {
     let fields = "";
 
@@ -26,4 +15,10 @@ function generateMeta(obj) {
 */`;
 }
 
-build(args);
+await esbuild.build({
+    watch: args.includes("--dev"),
+    banner: {
+        js: generateMeta(config.meta)
+    },
+    ...config.esbuild
+});
